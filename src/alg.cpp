15f2934317802d0e9dd6a4d0d16ab0bf6e6f97f5
NapylovaDariaAleksandrivna <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-03-25 16:12:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,13 +26,13 @@ int getPrior(char symbol) {
 std::string infx2pstfx(std::string inf) {
   TStack<char, 100> stack;
   std::string output = "";
-  int prior=0;
+  int prior = 0;
   int priorOnHeight = -1;
   for (auto& op : inf) {
-    prior = getPrior (op);
-    priorOnHeight = getPrior (stack.get());
+    prior = getPrior(op);
+    priorOnHeight = getPrior(stack.get());
     if (op == '\n') { /* 1. Esli konets stroki*/
-      while (getPrior (stack.get())>1) {
+      while(getPrior (stack.get()) > 1) {
         output += stack.pop();
         output += " ";
       }
@@ -41,10 +41,10 @@ std::string infx2pstfx(std::string inf) {
     if (prior == -1) { /* 2. Esli chislo*/
       output += op;
       output += " ";
-    } else if (prior == 0){ /* 3. Skobka ( */
+    } else if (prior == 0) { /* 3. Skobka ( */
       stack.push(op);
     } else if (prior == 1) { /* 4. Esli )*/
-      while (getPrior (stack.get())>0) {
+      while (getPrior(stack.get()) > 0) {
        output += stack.pop();
        output += " ";
       }
@@ -52,20 +52,20 @@ std::string infx2pstfx(std::string inf) {
     } else if (prior > priorOnHeight || stack.isEmpty()) {
       stack.push(op);
     } else if (prior <= priorOnHeight && prior > 1) {
-      while (getPrior (stack.get())>1) {
+      while (getPrior(stack.get()) > 1) {
         output += stack.pop();
         output += " ";
       }
       stack.push(op);
     }
   }
-  while (getPrior (stack.get())>1) {
+  while (getPrior(stack.get()) > 1) {
       output += stack.pop();
       output += " ";
   }
   return output;
 }
-int operatoR (int a, int b, char op){
+int operatoR(int a, int b, char op) {
   switch (op) {
   case '+':
     return a+b;
@@ -80,23 +80,22 @@ int operatoR (int a, int b, char op){
 }
 int eval(std::string pref) {
   TStack<int, 100> stack1;
-  int per=0;
+  int per = 0;
   int prior = 0;
-  for (auto& op:pref) {
-    prior = getPrior (op);
-    if (prior == -100){ //space
+  for (auto& op : pref) {
+    prior = getPrior(op);
+    if (prior == -100) { //space
       continue;
     }
     if (prior == -1) { //number
       stack1.push(op-48);
-    }
-    else {
+    } else {
       int hz=0;
-      while (!(stack1.isEmpty()+hz)){
-          hz+=1;
+      while (!(stack1.isEmpty()+hz)) {
+          hz += 1;
       int a = stack1.pop();
       int b = stack1.pop();
-      per = operatoR (b, a, op);
+      per = operatoR(b, a, op);
       stack1.push(per);
       }
     }
